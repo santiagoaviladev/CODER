9e1e0b27b622a7aea80a569412df2451baf5c11d
Santiago Avila <xavier0823@gmail.com>
2022-03-14 23:57:51 -0500

    Template Clase Operadores Avanzados

---
+++ CLASEOPERADORESAVANZADOS/15EJERCICIOFINALTEMPLATE/functions/main.js
@@ -0,0 +1,37 @@
+function init()
+{
+    cargarCategorias();
+   
+}
+
+
+/* Escriba la funcion cargarCategorias
+    Las categorías deben estar en botones (<button>)
+    Cada botón debe tener una acción que al oprimirlo, se carguen los productos asociados
+    (cargarProductos)
+*/
+
+function cargarCategorias()
+{
+    
+
+}
+
+/*
+    La función cargar productos recibe una categoría (OBJETO) como parámetro y muestra el listado de productos 
+    Use elementos UL Y LI para cada producto de la cetagoría 
+    Use un Elemento <h4>Productos de {categoria}</h4> como título del contenedor
+    Para validar la integridad de la información, use operadores avanzados, en donde aplique
+    Cada Producto debe mostrar:    
+        Nombre (en un <h5>)
+        Precio 
+        Si el producto tiene descuento, debe mostrar el descuento
+        Si el producto no tiene descuento, debe mostrar el texto 'Sin descuento'
+    Si el producto tiene inventario, debe mostrar el botón 'Comprar' (NO DEBE PROGRAMAR LA FUNCIONALIDAD DE ESTE BOTÓN)
+
+*/
+function cargarProductos(categoria)
+{
+    //TODO
+}
+
