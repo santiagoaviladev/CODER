fcbc2db04d78c84e5a2a9fa365d01b5844cc74ec
santiagoaviladev <79491659+santiagoaviladev@users.noreply.github.com>
2022-03-15 09:22:22 -0500

    Update main.js

--- CLASEOPERADORESAVANZADOS/15EJERCICIOFINALTEMPLATE/functions/main.js
+++ CLASEOPERADORESAVANZADOS/15EJERCICIOFINALTEMPLATE/functions/main.js
@@ -9,6 +9,8 @@ function init()
     Las categorías deben estar en botones (<button>)
     Cada botón debe tener una acción que al oprimirlo, se carguen los productos asociados
     (cargarProductos)
+    
+    Utilice destructuring, para obtner las propiedades del obejeto categoría, cuando las vaya a mostrar en pantalla.
 */
 
 function cargarCategorias()
@@ -28,6 +30,8 @@ function cargarCategorias()
         Si el producto tiene descuento, debe mostrar el descuento
         Si el producto no tiene descuento, debe mostrar el texto 'Sin descuento'
     Si el producto tiene inventario, debe mostrar el botón 'Comprar' (NO DEBE PROGRAMAR LA FUNCIONALIDAD DE ESTE BOTÓN)
+    
+    Utilice destructuring, para obtner las propiedades del obejeto producto, cuando las vaya a mostrar en pantalla.
 
 */
 function cargarProductos(categoria)
